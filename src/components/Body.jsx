debbb1a9ad4a354fb047c656c75edf9734c052e1
abhi-jith-123 <abhijithkannan905@gmail.com>
2023-09-14 14:07:29 +0530

    first commit

---
+++ src/components/Body.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Body() {
+  return (
+    <div>
+      <h1 style={{backgroundColor:"green"}}>body</h1>
+    </div>
+  )
+}
+
+export default Body
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+      <h1 style={{backgroundColor:"yellow"}}>this is footeer</h1>
+    </div>
+  )
+}
+
+export default Footer
